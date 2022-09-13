1e7c68738d1575e1ff96ede5612fbafcc0560f9b
tdechaize <76624404+tdechaize@users.noreply.github.com>
2022-09-13 12:34:16 +0200

    Update generate_all_with_code_blocks.bat

--- generate_all_with_code_blocks.bat
+++ generate_all_with_code_blocks.bat
@@ -3,29 +3,27 @@ REM ----------------------------------------------------------------------------
 REM
 REM		 generate_all_with_code_blocks.bat : 	Nom de ce batch  
 REM
-REM      Batch de lancement de toutes les générations d'une application Windows (source C avec un fichier resource) 
+REM      Batch de lancement de toutes les gÃ©nÃ©rations d'une application Windows (source C avec un fichier resource) 
 REM    avec l'IDE Code::Blocks en mode ligne de commande,
-REM    ou d'une seule génération, si le troisième paramètre fait partie de la liste suivante :
+REM    ou d'une seule gÃ©nÃ©ration, si le troisiÃ¨me paramÃ¨tre reprend une ces "real target" dÃ©finie dans le projet CB (exemple : "Debug DMC").
 REM    
-REM          [BCC|MINGW32OF|MINGW64CB|DEVCPP|CYGWIN32|CYGWIN64|MINGW32WL|MINGW64WL|TDM32|TDM64|MSYS2W32|MSYS2W64|NMAKEX32|NMAKEX64|VS2022X32|VS2022X64|CLANGX32|CLANGX64|CLANGW32|CLANGW64|CLANGMW32|CLANGMW64|DMC|LCC32|LCC64|PELLESC32|PELLESC64|WATCOM32|WATCOM64] 
+REM     PrÃ©-requis, il faut bien entendu, positionner la variable d'environnement PATH en y incluant le rÃ©pertoire d'installation de l'IDE Code::Blocks.
+REM     L'IDE C::B est capable de gÃ©nÃ©rer TOUtes les gÃ©nÃ©rations par l'intermÃ©diaire d'une "virtual target" dÃ©nommÃ© "All build".
+REM     Points d'attention, j'ai positionnÃ© des variables d'environnement sous Windows (en mode "systÃ¨me") pour gÃ©rer les diffÃ©rentes versions de Visual Studio, du KIT WINDOWS et de CLANG installees :
+REM          CLANG_VERSION     valuÃ© (Ã  date) par       14.0.6     		(derniÃ¨re version sur Windows 11, aussi bien pour les binaires valables pour VS2022 que pour les environnements Mingw et MSYS)
+REM          VS_VERSION        valuÃ© (Ã  date) par       2022       		(derniÃ¨re version sur Windows 11)
+REM          VS_NUM            valuÃ© (Ã  date) par       14.33.31629     (derniÃ¨re version sur Windows 11)
+REM          KIT_WIN_VERSION   valuÃ© (Ã  date) par       10    			(derniÃ¨re version sur Windows 11)
+REM          KIT_WIN_NUM       valuÃ© (Ã  date) par       10.0.22621.0    (derniÃ¨re version sur Windows 11)
+REM		Je les utilise par l'opÃ©rateur de traduction %var% des fichiers de commandes Windows.
 REM
-REM     Pré-requis, il faut bien entendu, positionner la variable d'environnement PATH en y incluant le répertoire d'installation de l'IDE Code::Blocks.
-REM     L'IDE C::B est capable de générer TOUtes les générations par l'intermédiaire d'une "virtual target" dénommé "all build".
-REM     Points d'attention, j'ai positionné des variables d'environnement sous Windows (en mode "système") pour gérer les différentes versions de Visual Studio, du KIT WINDOWS et de CLANG installees :
-REM          CLANG_VERSION     valué (à date) par       14.0.6     		(dernière version sur Windows 11, aussi bien pour les binaires valables pour VS2022 que pour les environnements Mingw et MSYS)
-REM          VS_VERSION        valué (à date) par       2022       		(dernière version sur Windows 11)
-REM          VS_NUM            valué (à date) par       14.33.31629     (dernière version sur Windows 11)
-REM          KIT_WIN_VERSION   valué (à date) par       10    			(dernière version sur Windows 11)
-REM          KIT_WIN_NUM       valué (à date) par       10.0.22621.0    (dernière version sur Windows 11)
-REM		Je les utilise par l'opérateur de traduction %var% des fichiers de commandes Windows.
-REM
-REM 	PS : la procédure "create_dir.bat" permet de créer TOUS les répertoires utiles à ces générations multiples (certains compilateurs ne sont pas caapbles de les créer ONLINE s'ils sont absents),
-REM          et ensuite on lance generate_all_with_code_blocks.bat "nom_répertoire" "nom_appli" [id_generator].
+REM 	PS : la procÃ©dure "create_dir.bat" permet de crÃ©er TOUS les rÃ©pertoires utiles Ã  ces gÃ©nÃ©rations multiples (certains compilateurs ne sont pas caapbles de les crÃ©er ONLINE s'ils sont absents),
+REM          et ensuite on lance generate_all_with_code_blocks.bat "nom_rÃ©pertoire" "nom_appli" [id_generator].
 REM 
 REM 	AUTHOR : 						Thierry DECHAIZE
-REM     Date de création :				9 août 2022   
-REM 	Date dernière modification : 	9 septembre 2022   -> adjonction d'un deuxième paramètre recupéré dans la variable %NAME_APPLI% pour augmenter le paramètrage de ce script.
-REM 	Détails des modifications : 	Ce deuxième paramètre décale par conséquent le troisième (toujours optionnel) qui continue de servir de choix du générateur/compilateur souhaité pour tests.
+REM     Date de crÃ©ation :				9 aoÃ»t 2022   
+REM 	Date derniÃ¨re modification : 	9 septembre 2022   -> adjonction d'un deuxiÃ¨me paramÃ¨tre recupÃ©rÃ© dans la variable %NAME_APPLI% pour augmenter le paramÃ¨trage de ce script.
+REM 	DÃ©tails des modifications : 	Ce deuxiÃ¨me paramÃ¨tre dÃ©cale par consÃ©quent le troisiÃ¨me (toujours optionnel) qui continue de servir de choix du gÃ©nÃ©rateur/compilateur souhaitÃ© pour tests.
 REM 	Version de ce script :			1.1.0 ->  "Version majeure" . "Version mineure" . "niveau de patch"
 REM
 REM ---------------------------------------------------------------------------------------------------
@@ -50,16 +48,16 @@ SET PATH=C:\CodeBlocks;%PATH%
 :SUITE
 if "%3" NEQ "" goto CBONE else set TARGET=%3
 
-REM             Lancement Code::Blocks de toutes les générations 
+REM             Lancement Code::Blocks de toutes les gÃ©nÃ©rations 
 :CBALL
 codeblocks.exe /na /nd --no-splash-screen --batch-build-notify --no-batch-window-close --rebuild %NAME_APPLI%.workspace --target='All_build' 
 cd %SOURCE_DIR%
 SET PATH=%PATHSAV%
 GOTO FIN
 
-REM             Lancement Code::Blocks dpour une seule génération désignée par "target"
+REM             Lancement Code::Blocks dpour une seule gÃ©nÃ©ration dÃ©signÃ©e par "target"
 :CBONE
-codeblocks.exe /na /nd --no-splash-screen --batch-build-notify --no-batch-window-close --rebuild %NAME_APPLI%.workspace --target=’%TARGET%’ 
+codeblocks.exe /na /nd --no-splash-screen --batch-build-notify --no-batch-window-close --rebuild %NAME_APPLI%.workspace --target=â€™%TARGET%â€™ 
 cd %SOURCE_DIR%
 SET PATH=%PATHSAV%
 GOTO FIN
@@ -67,7 +65,7 @@ GOTO FIN
 :usage
 echo Usage : %0 DIRECTORY_SRC NAME_APPLI [Generateur] 
 echo   avec compilateur = [BCC|MINGW32OF|MINGW64CB|DEVCPP|CYGWIN32|CYGWIN64|MINGW32WL|MINGW64WL|TDM32|TDM64|MSYS2W32|MSYS2W64|NMAKEX32|NMAKEX64|VS2022X32|VS2022X64|CLANGX32|CLANGX64|CLANGW32|CLANGW64|CLANGMW32|CLANGMW64|DMC|LCC32|LCC64|PELLESC32|PELLESC64|WATCOM32|WATCOM64]
-echo   et si pas de troisième paramètre, exécution via CB de toutes les générations.
+echo   et si pas de troisiÃ¨me paramÃ¨tre, exÃ©cution via CB de toutes les gÃ©nÃ©rations.
 
 :FIN
 cd %DIRINIT%
